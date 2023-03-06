a80500cb315c3a914d809d8df2a0db39889d2168
Wendell <41480456+WendellXY@users.noreply.github.com>
2023-03-06 10:04:54 +0800

    support update config

--- Sources/SuperGoodsProcessorCLI/SuperGoodsProcessorCLI.swift
+++ Sources/SuperGoodsProcessorCLI/SuperGoodsProcessorCLI.swift
@@ -21,7 +21,37 @@ struct SuperGoodsProcessorCLI: AsyncParsableCommand {
             GoodsCommand.self,
             GoodsDetailsCommand.self,
             PromotionCommand.self,
-        ],
-        defaultSubcommand: GoodsCommand.self
+        ]
     )
+
+    @Flag(name: [.customLong("init"), .short], help: "Initialize Basic Environment")
+    var initialize = false
+
+    init() {
+        guard let data = try? Data(contentsOf: .defaultResultSavingDirectory.appendingPathComponent("config.json")) else { return }
+        if let config = try? JSONDecoder().decode(Configuration.self, from: data) {
+            PDDService.shared.config = config
+        }
+    }
+
+    static func initializeEnvironment() throws {
+        try FileManager.default.createDirectory(at: .defaultResultSavingDirectory, withIntermediateDirectories: true, attributes: nil)
+
+        // Create a sample configuration
+        let sampleConfiguration = Configuration.sample
+
+        let jsonEncoder = JSONEncoder()
+        jsonEncoder.outputFormatting = .prettyPrinted
+
+        let sampleConfigData = try jsonEncoder.encode(sampleConfiguration)
+
+        // Save the configuration to a file
+        try sampleConfigData.write(to: .defaultResultSavingDirectory.appendingPathComponent("config.json"))
+    }
+
+    mutating func run() async throws {
+        if initialize {
+            try Self.initializeEnvironment()
+        }
+    }
 }
