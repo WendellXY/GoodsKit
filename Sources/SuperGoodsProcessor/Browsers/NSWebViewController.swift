7a4a64bc030d77b1d272232e0d932f2fe2bb9954
Wendell <41480456+WendellXY@users.noreply.github.com>
2023-03-07 10:38:31 +0800

    updated indent

--- Sources/SuperGoodsProcessor/Browsers/NSWebViewController.swift
+++ Sources/SuperGoodsProcessor/Browsers/NSWebViewController.swift
@@ -8,45 +8,45 @@
 
 #if os(macOS)
 
-    import AppKit
-    import WebKit
+import AppKit
+import WebKit
 
-    public final class NSWebViewController: NSViewController, SGPBrowserWrapper {
+public final class NSWebViewController: NSViewController, SGPBrowserWrapper {
 
-        public override func loadView() {
-            let config = WKWebViewConfiguration()
-            let webView = WKWebView(frame: .zero, configuration: config)
-            webView.navigationDelegate = self
+    public override func loadView() {
+        let config = WKWebViewConfiguration()
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.navigationDelegate = self
 
-            self.view = webView
-        }
-
-        // swiftlint:disable:next force_cast
-        public var webView: WKWebView { self.view as! WKWebView }
-
-        public var goodsDataSet: Set<GoodsDetailData> = []
-        public var noTagListURL: [URL] = []
-        public var goodsID = 0
-
-        public var kindIndex = 0
-        public var itemIndex = 0
-
-        public override func viewDidLoad() {
-            super.viewDidLoad()
-            startLoop()
-        }
+        self.view = webView
     }
 
-    extension NSWebViewController: WKNavigationDelegate {
-        public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            Task {
-                do {
-                    try await parseFromWebView(webView)
-                } catch {
-                    print(error)
-                }
+    // swiftlint:disable:next force_cast
+    public var webView: WKWebView { self.view as! WKWebView }
+
+    public var goodsDataSet: Set<GoodsDetailData> = []
+    public var noTagListURL: [URL] = []
+    public var goodsID = 0
+
+    public var kindIndex = 0
+    public var itemIndex = 0
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        startLoop()
+    }
+}
+
+extension NSWebViewController: WKNavigationDelegate {
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        Task {
+            do {
+                try await parseFromWebView(webView)
+            } catch {
+                print(error)
             }
         }
     }
+}
 
 #endif
