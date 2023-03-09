401a16cf103af8785ab4796280588f2a85bc5524
Wendell <41480456+WendellXY@users.noreply.github.com>
2023-03-09 17:05:09 +0800

    add doc

--- Sources/GoodsKit/Browsers/SGPBrowsersConfiguration.swift
+++ Sources/GoodsKit/Browsers/SGPBrowsersConfiguration.swift
@@ -11,6 +11,9 @@ import Foundation
 public struct SGPBrowsersConfiguration {
     public static var goodsKinds: [String] = []
     public static var goodsList: [[GoodsData]] = []
+    /// The maximum number of times the browser will try to load the same page. The default value is 3, it is suggest to keep it at 3 or above.
+    ///
+    /// If the browser fails to load the page for more than this number of times, it will move on to the next page.
     public static var maxRepeatedTimes = 3
     public static var productURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
 }
